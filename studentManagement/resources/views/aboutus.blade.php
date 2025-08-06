aa15e9777c8228ec90cfaea21ba09f1e7c65e88d
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-08-06 21:45:32 +0700

    Laravel 12 Blade Directives – Master Templating Like a Pro

--- studentManagement/resources/views/aboutus.blade.php
+++ studentManagement/resources/views/aboutus.blade.php
@@ -8,9 +8,23 @@
 </head>
 
 <body>
+    {{--
+    this is a comment
+    this is a second comment
+    --}}
+
+    <!-- This is a HTML comment -->
     <h1>About Us</h1>
     <h2>Name: {{ $name }}</h2>
     <h2>ID: {{ $id }}</h2>
+
+    @for($i=0; $i < 10; $i++)
+        <p>Item {{ $i }}</p>
+
+        @if($i == 5)
+            <h1>Hi this is {{ $i }}</h1>
+        @endif
+    @endfor
 </body>
 
 </html>

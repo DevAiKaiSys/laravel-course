07cd7339005d1bbf68af36173100f4e595f5a646
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-08-06 21:06:58 +0700

    Laravel 12 – Passing Data from Route to View Explained

--- studentManagement/resources/views/contectus.blade.php
+++ studentManagement/resources/views/contectus.blade.php
@@ -1,4 +1,6 @@
 <div>
     <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
-     <h>Contact Us</h1>
+    <h2>Contact Us</h2>
+    <h2>Name: {{ request()->name }}</h2>
+    <h2>ID: {{ request()->id }}</h2>
 </div>
--- studentManagement/routes/web.php
+++ studentManagement/routes/web.php
@@ -6,8 +6,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about-us', function () {
-    return view('aboutus');
+// Route::get('/about-us', function () {
+//     /* $name='tester1';
+//     $email='tester1@gmail.com';
+//     return view('aboutus')->with('name', $name)->with('email', $email); */
+//     /* $name='tester2';
+//     $email='tester2@gmail.com';
+//     return view('aboutus', compact('name', 'email')); */
+//     $name = 'tester3';
+//     $email = 'tester3@gmail.com';
+//     return view('aboutus', ['name' => $name, 'email' => $email]);
+// });
+Route::get('/about-us/{name}/{id}', function ($name, $id) {
+    return view('aboutus', compact('name', 'id'));
 });
 
-Route::view('contect-us', 'contectus');
+/* Route::view('contect-us', 'contectus', ['name' => 'tester', 'email' => 'tester@gmail.com']); */
+Route::view('contect-us/{name}/{id}', 'contectus');

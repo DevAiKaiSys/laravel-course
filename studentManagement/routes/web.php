24e618bcbcc655af645607f58602ba64fec8de80
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-08-06 20:22:18 +0700

    Laravel 12 Route Grouping & Naming – Organize Your Routes Like a Pro

--- studentManagement/routes/web.php
+++ studentManagement/routes/web.php
@@ -2,6 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'hello from laravel';
+});
+
+Route::get('/about', function () {
+    return 'About Us';
+});
+
+// Route::get('/details/students', function () {
+//     return 'this is students';
+// });
+
+// Route::get('/details/teachers', function () {
+//     return 'this is teachers';
+// });
+
+Route::prefix('details')->group(function () {
+    Route::get('/students', function () {
+        return 'this is students';
+    })->name('students-details');
+
+    Route::get('/teachers', function () {
+        return 'this is teachers';
+    })->name('teachers-details');
 });

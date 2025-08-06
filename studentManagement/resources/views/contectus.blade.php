075e6c035fa384137c8ff750a942fa3e37efed61
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-08-06 20:40:52 +0700

    Laravel 12 View Explained – Introduction to Blade Templates

---
+++ studentManagement/resources/views/contectus.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+     <h>Contact Us</h1>
+</div>
--- studentManagement/routes/web.php
+++ studentManagement/routes/web.php
@@ -2,40 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-    return 'hello from laravel';
+    return view('welcome');
 });
 
-Route::get('/about', function () {
-    return 'About Us';
+Route::get('/about-us', function () {
+    return view('aboutus');
 });
 
-// Route::get('/details/students', function () {
-//     return 'this is students';
-// });
-
-// Route::get('/details/teachers', function () {
-//     return 'this is teachers';
-// });
-
-Route::prefix('details')->group(function () {
-    Route::get('/students', function () {
-        return 'this is students';
-    })->name('students-details');
-
-    Route::get('/teachers', function () {
-        return 'this is teachers';
-    })->name('teachers-details');
-});
-
-Route::get('/students/{id}/{reg}',function ($id, $reg) {
-    return 'this is student number: '.$id.' registration number: '.$reg;
-});
-
-Route::fallback(function () {
-    return 'This page is not found please try again';
-});
+Route::view('contect-us', 'contectus');

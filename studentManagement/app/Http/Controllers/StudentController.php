74d2b2448df33d7dd5db909253ab34dada2e790a
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-08-07 22:07:09 +0700

    Laravel 12 – Public vs Private vs Constructor Methods in Controllers

--- studentManagement/app/Http/Controllers/StudentController.php
+++ studentManagement/app/Http/Controllers/StudentController.php
@@ -8,6 +8,15 @@ class StudentController extends Controller
 {
     //
 
+    protected $name;
+    private $age;
+
+    public function __construct()
+    {
+        $this->name = 'My Name';
+        $this->age = 20;
+    }
+
     public function index()
     {
         return 'hello from StudentController';
@@ -20,6 +29,17 @@ class StudentController extends Controller
     public function aboutUs($id, $name)
     {
         /* return 'ID No ' . $id . ' and Name is ' . $name; */
+
+        /* $name = $this->privateFunction();
+        return $name; */
+        /* return $this->name; */
+        return $this->age;
+
         return view('aboutus', compact('id', 'name'));
+    }
+
+    private function privateFunction()
+    {
+        return 'hello world';
     }
 }

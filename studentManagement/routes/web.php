cc62ab90243917007bddd12a1ec778c2a5b9ab54
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-08-07 23:09:26 +0700

    Laravel 12 – Introduction to Models: Working with Database Records

--- studentManagement/routes/web.php
+++ studentManagement/routes/web.php
@@ -3,43 +3,15 @@
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\SecondTestController;
+use App\Http\Controllers\TeachersController;
+use App\Models\Teacher;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/about-us', function () {
-//     /* $name='tester1';
-//     $email='tester1@gmail.com';
-//     return view('aboutus')->with('name', $name)->with('email', $email); */
-//     /* $name='tester2';
-//     $email='tester2@gmail.com';
-//     return view('aboutus', compact('name', 'email')); */
-//     $name = 'tester3';
-//     $email = 'tester3@gmail.com';
-//     return view('aboutus', ['name' => $name, 'email' => $email]);
-// });
-/* Route::get('/about-us/{name}/{id}', function ($name, $id) {
-    return view('aboutus', compact('name', 'id'));
+/* Route::get('/teachers', function () {
+    return Teacher::all();
 }); */
-/* Route::get('/about-us', function () {
-    return view('aboutus');
-}); */
-
-/* Route::view('contect-us', 'contectus', ['name' => 'tester', 'email' => 'tester@gmail.com']); */
-/* Route::view('contect-us/{name}/{id}', 'contectus'); */
-Route::view('/contact-us', 'contactus');
-
-/* Route::get('/students', [StudentController::class, 'index']);
-Route::get('/about-us', [StudentController::class, 'aboutUs']); */
-Route::controller(StudentController::class)->group(function () {
-    Route::get('students', 'index');
-    Route::get('about-us', 'aboutUs');
-    /* Route::get('about-us/{id}/{name}', 'aboutUs'); */
-});
-/* Route::get('about-us/{id}/{name}', [StudentController::class, 'aboutUs']); */
-
-Route::get('invoke', TestController::class);
-
-Route::resource('second-test', SecondTestController::class);
+Route::get('/teachers', [TeachersController::class, 'index']);

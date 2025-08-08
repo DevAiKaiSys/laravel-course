20d11d73360a8968c257f6725b2c77cdeb8b68f9
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-08-08 07:29:31 +0700

    Laravel 12 – Basic CRUD Operations Using Models

--- studentManagement/app/Http/Controllers/TeachersController.php
+++ studentManagement/app/Http/Controllers/TeachersController.php
@@ -12,4 +12,37 @@ class TeachersController extends Controller
     {
         return Teachers::all();
     }
+
+    public function add()
+    {
+        $teacher = new Teachers();
+        $teacher->name = 'Test Name';
+        $teacher->save();
+
+        return 'Added Successfully';
+    }
+
+    public function show($id)
+    {
+        $item = Teachers::findOrFail($id);
+
+        return $item;
+    }
+
+    public function update($id)
+    {
+        $teacher = Teachers::findOrFail($id);
+        $teacher->name = 'Updated Teacher';
+        $teacher->save();
+
+        return 'Updated Successfully';
+    }
+
+    public function delete($id)
+    {
+        $teacher = Teachers::findOrFail($id);
+        $teacher->delete();
+
+        return 'Deleted Successfully';
+    }
 }
--- studentManagement/routes/web.php
+++ studentManagement/routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', function () {
     return Teacher::all();
 }); */
 Route::get('/teachers', [TeachersController::class, 'index']);
+Route::get('/add-teacher', [TeachersController::class, 'add']);
+Route::get('/show-teacher/{id}', [TeachersController::class, 'show']);
+Route::get('/update-teacher/{id}', [TeachersController::class, 'update']);
+Route::get('/delete-teacher/{id}', [TeachersController::class, 'delete']);
